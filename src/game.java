95fe725ac399edd4f1cdea7a31667225243fb889
Benjamin Lindeen <lind1669@umn.edu>
2023-07-26 15:34:38 -0500

    update game

--- src/game.java
+++ src/game.java
@@ -1,8 +1,11 @@
 public class game {
     public static void main(String[] args) {
-        prize doorOne = new prize();
-        prize doorTwo = new prize();
-        prize doorThree = new prize();
+        prizeBool doorOne = new prizeBool();
+        prizeBool doorTwo = new prizeBool();
+        prizeBool doorThree = new prizebool();
 
+        System.out.println("Door one is a " + doorOne.whichPrize);
+        System.out.println("Door two is a " + doorTwo.whichPrize);
+        System.out.println("Door three is a " + doorThree.whichPrize);
     }
 }
