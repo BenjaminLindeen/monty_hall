6262a8c7283888d5770079624989610a8a8eaf12
Benjamin Lindeen <lind1669@umn.edu>
2023-07-27 18:25:45 -0500

    game

--- src/game.java
+++ src/game.java
@@ -1,9 +1,26 @@
+import java.util.Random;
+
 public class game {
-    public static void main(String[] args) {
-        prizeBool doorOne = new prizeBool();
-        prizeBool doorTwo = new prizeBool();
-        prizeBool doorThree = new prizeBool();
+    private static final Random random = new Random();
+    public static boolean[] doors() {
+        boolean[] doors = {false, false, false};
+        doors[random.nextInt(3)] = true;
+        return doors;
+    }
 
+    public static int chooseDoor(boolean[] doors) {
+        return random.nextInt(3);
+    }
 
+    public static int openDoor(boolean[] doors, int chooseDoor) {
+        if (chooseDoor == 0) {
+            return random.nextInt(2) + 1;
+        } else if (chooseDoor == 1) {
+            int rand = random.nextInt(2);
+            return (rand == 0) ? 0 : 2;
+        } else if (chooseDoor == 2) {
+            return random.nextInt(2);
+        }
+        return - 1;
     }
 }
---
+++ src/switchPrize.java
@@ -0,0 +1,9 @@
+public class switchPrize {
+    public static boolean switchWin () {
+        if (game.openDoor(game.doors(), game.chooseDoor(game.doors())) == 0) {
+
+        } else {
+            return false;
+        }
+    }
+}
